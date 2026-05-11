64ef4eb711c4e352855af5bf3bc55c7aaa0b4b18
Oleksandr <mimo3a@gmail.com>
2026-05-11 22:33:52 +0200

    pwm speed v_1_0

--- my_robot.py
+++ my_robot.py
@@ -5,9 +5,11 @@ print("START")
 
 GPIO.setmode(GPIO.BCM)
 
+
 class Moove:
 
     def __init__(self, in1=17, in2=27, in3=22, in4=23):
+
         self.in1 = in1
         self.in2 = in2
         self.in3 = in3
@@ -18,35 +20,93 @@ class Moove:
         GPIO.setup(self.in3, GPIO.OUT)
         GPIO.setup(self.in4, GPIO.OUT)
 
-    def forward(self):
-        GPIO.output(self.in1, GPIO.HIGH)
-        GPIO.output(self.in2, GPIO.LOW)
+        # PWM
+        self.pwm1 = GPIO.PWM(self.in1, 1000)
+        self.pwm2 = GPIO.PWM(self.in2, 1000)
+        self.pwm3 = GPIO.PWM(self.in3, 1000)
+        self.pwm4 = GPIO.PWM(self.in4, 1000)
 
-        GPIO.output(self.in3, GPIO.HIGH)
-        GPIO.output(self.in4, GPIO.LOW)
+        self.pwm1.start(0)
+        self.pwm2.start(0)
+        self.pwm3.start(0)
+        self.pwm4.start(0)
 
-    def backward(self):
-        GPIO.output(self.in1, GPIO.LOW)
-        GPIO.output(self.in2, GPIO.HIGH)
+    def forward(self, speed=50):
 
-        GPIO.output(self.in3, GPIO.LOW)
-        GPIO.output(self.in4, GPIO.HIGH)
+        self.pwm1.ChangeDutyCycle(speed)
+        self.pwm2.ChangeDutyCycle(0)
+
+        self.pwm3.ChangeDutyCycle(speed)
+        self.pwm4.ChangeDutyCycle(0)
+
+    def backward(self, speed=50):
+
+        self.pwm1.ChangeDutyCycle(0)
+        self.pwm2.ChangeDutyCycle(speed)
+
+        self.pwm3.ChangeDutyCycle(0)
+        self.pwm4.ChangeDutyCycle(speed)
+
+    def left(self, speed=50):
+
+        self.pwm1.ChangeDutyCycle(0)
+        self.pwm2.ChangeDutyCycle(0)
+
+        self.pwm3.ChangeDutyCycle(speed)
+        self.pwm4.ChangeDutyCycle(0)
+
+    def right(self, speed=50):
+
+        self.pwm1.ChangeDutyCycle(speed)
+        self.pwm2.ChangeDutyCycle(0)
+
+        self.pwm3.ChangeDutyCycle(0)
+        self.pwm4.ChangeDutyCycle(0)
 
     def stop(self):
-        GPIO.output(self.in1, GPIO.LOW)
-        GPIO.output(self.in2, GPIO.LOW)
-        GPIO.output(self.in3, GPIO.LOW)
-        GPIO.output(self.in4, GPIO.LOW)
+
+        self.pwm1.ChangeDutyCycle(0)
+        self.pwm2.ChangeDutyCycle(0)
+        self.pwm3.ChangeDutyCycle(0)
+        self.pwm4.ChangeDutyCycle(0)
+
+    def cleanup(self):
+        self.stop()
+
+        self.pwm1.stop()
+        self.pwm2.stop()
+        self.pwm3.stop()
+        self.pwm4.stop()
+
+        self.pwm1 = None
+        self.pwm2 = None
+        self.pwm3 = None
+        self.pwm4 = None
+
+        time.sleep(0.1)
+
+        GPIO.cleanup()
 
 
 robot = Moove()
 
-robot.forward()
+# Вперёд медленно
+robot.forward(40)
 time.sleep(2)
 
-robot.backward()
+# Вперёд быстро
+robot.forward(80)
 time.sleep(2)
 
+# Поворот влево
+robot.left(60)
+time.sleep(1)
+
+# Назад
+robot.backward(50)
+time.sleep(2)
+
+# Стоп
 robot.stop()
 
-GPIO.cleanup()
+robot.cleanup()
