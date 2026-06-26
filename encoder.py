d5c44d72320bfd3d5cc289555af4b2ceffe50fc2
Oleksandr <mimo3a@gmail.com>
2026-06-26 20:26:29 +0200

    encoder test speed

--- encoder.py
+++ encoder.py
@@ -1,39 +1,23 @@
-import time
 import RPi.GPIO as GPIO
 
-LEFT_ENCODER = 24
-RIGHT_ENCODER = 25
 
-left_count = 0
-right_count = 0
+class Encoder:
+    def __init__(self, pin):
+        self.pin = pin
+        self.count = 0
 
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(
+            self.pin,
+            GPIO.FALLING,
+            callback=self._pulse
+        )
 
-def left_pulse(channel):
-    global left_count
-    left_count += 1
+    def _pulse(self, channel):
+        self.count += 1
 
+    def read(self):
+        return self.count
 
-def right_pulse(channel):
-    global right_count
-    right_count += 1
-
-
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(LEFT_ENCODER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(RIGHT_ENCODER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-GPIO.add_event_detect(LEFT_ENCODER, GPIO.FALLING, callback=left_pulse)
-GPIO.add_event_detect(RIGHT_ENCODER, GPIO.FALLING, callback=right_pulse)
-
-try:
-    while True:
-        left_count = 0
-        right_count = 0
-
-        time.sleep(1)
-
-        print("Left:", left_count, "Right:", right_count)
-
-finally:
-    GPIO.cleanup()
+    def reset(self):
+        self.count = 0
--- my_robot.py
+++ my_robot.py
@@ -1,13 +1,34 @@
 import time
+import RPi.GPIO as GPIO
 
 from motor import Motor
+from encoder import Encoder
 
+
+LEFT_ENCODER_PIN = 24
+RIGHT_ENCODER_PIN = 25
+
+GPIO.setmode(GPIO.BCM)
 
 motors = Motor()
+left_encoder = Encoder(LEFT_ENCODER_PIN)
+right_encoder = Encoder(RIGHT_ENCODER_PIN)
 
 try:
     motors.forward(40)
-    time.sleep(2)
-    motors.stop()
+
+    while True:
+        left_encoder.reset()
+        right_encoder.reset()
+
+        time.sleep(0.2)
+
+        left_speed = left_encoder.read()
+        right_speed = right_encoder.read()
+
+        print("Left:", left_speed, "Right:", right_speed)
+
 finally:
+    motors.stop()
     motors.cleanup()
+    GPIO.cleanup()
